c8c92bd759ad09e41c5fcec0ef189f8909be4eab
Arthur Melton <amtitan@icloud.com>
2021-09-20 07:36:46 -0400

    what I got

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,40 @@
 use curl::easy::Easy;
-use std::time::{SystemTime, UNIX_EPOCH, Duration};
-use serde_json::{json, Value, from_str};
-use std::ops::Index;
-use std::thread::sleep;
+use std::time::{SystemTime, UNIX_EPOCH};
+use serde_json::Value;
 
 fn main() {
-    let mut dates:Vec<String> = Vec::new();
-    let mut challenge:Vec<String> = Vec::new();
-    let mut difficult:Vec<String> = Vec::new();
-    let mut time = 0;
-    
-    let mut dst = Vec::new();
-    let mut easy = Easy::new();
-    easy.url(&["https://api.pushshift.io/reddit/search/submission/?subreddit=dailyprogrammer&sort=desc&sort_type=created_utc&after=", &time.to_string(), "&before=", SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards").as_millis().to_string().as_str(),"&size=1000"].join("")).unwrap();
-    let _output = easy.custom_request("GET");
-    let mut transfer = easy.transfer();
-    transfer
-        .write_function(|data| {
-            dst.extend_from_slice(data);
-            Ok(data.len())
-        })
-        .unwrap();
-    transfer.perform().unwrap();
-    drop(transfer);
-    
-    let v: Value = serde_json::from_str(&dst.iter().map(|&c| c as char).collect::<String>().as_str()).unwrap();
-    let sub_value: Vec<Value> = serde_json::from_value(v["data"].clone()).unwrap();
-    for item in &sub_value {
-        println!("{:?}", name(item["title"].to_string()));
+    let mut time:i64 = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards").as_millis().to_string().parse().unwrap();
+    let mut dates:Vec<&str> = Vec::new();
+    let mut stop = false;
+    while !stop {
+        let mut dst = Vec::new();
+        let mut easy = Easy::new();
+        easy.url(&["https://api.pushshift.io/reddit/search/submission/?subreddit=dailyprogrammer&sort=desc&sort_type=created_utc&after=0&before=", time.to_string().as_str(), "&size=1000"].join("")).unwrap();
+        let _output = easy.custom_request("GET");
+        let mut transfer = easy.transfer();
+        transfer
+            .write_function(|data| {
+                dst.extend_from_slice(data);
+                Ok(data.len())
+            })
+            .unwrap();
+        transfer.perform().unwrap();
+        drop(transfer);
+
+        let v: Value = serde_json::from_str(&dst.iter().map(|&c| c as char).collect::<String>().as_str()).unwrap();
+        let sub_value: Vec<Value> = serde_json::from_value(v["data"].clone()).unwrap();
+        for item in &sub_value {
+            if item["title"].to_string().chars().nth(1).unwrap() == '[' {
+                let name_out = name(item["title"].to_string()).to_owned();
+                dates.push(name_out[0].as_str());
+                if !dates.contains(&name_out[0].as_str()) {
+                    println!("{:?}", name_out);
+                } else {
+                    stop = true;
+                }
+            }
+        }
+        time = v["data"][0]["created_utc"].to_string().parse::<i64>().unwrap() - 1;
     }
 }
 
